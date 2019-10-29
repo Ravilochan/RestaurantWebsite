7e430ccba28fe09d3bf74532a10a51928da66934
Ravilochan2000 <ravilochan2000@gmail.com>
2019-10-29 06:29:28 +0530

    29oct

---
+++ server.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+$username=$_SESSION['username'];
+
+//error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+ $conn=mysqli_connect("localhost","root","","chef");
+
+$des=mysqli_query($conn,"SELECT `designation` FROM `users` WHERE username='$username'");
+$data= mysqli_fetch_array($des);
+$designation=$data['designation'];
+
+
+$totalbill=$_POST['total'];
+$customer_money=$_POST['money'];
+$retrun_money=$_POST['return_money'];
+$timing=date("d-m-Y h:i:sa");
+echo "$timing";
+
+
+
+$particulars =" Dosa : ".$_POST['quantity1']."  Idli : ".$_POST['quantity2']."  Bajji : ".$_POST['quantity3']."  Wada : ".$_POST['quantity4']."  Puri : ".$_POST['quantity5'] ;
+echo "$particulars";
+
+
+
+$query = "INSERT INTO bill( username, timing , designation, particulars , totalbill ,customer_money , return_money ) 
+VALUES('$username','$timing', '$designation', '$particulars','$totalbill','$customer_money','$retrun_money')";
+
+if(mysqli_query($conn,$query))
+{
+echo "entered database";
+}
+else{
+    echo "Not entered database";
+}
+
+header("location:bill.html");
+?>
